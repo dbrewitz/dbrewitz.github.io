d32484a88bd1e3cb62f5bd0986fc864f8fee0903
David <dbrewitz@users.noreply.github.com>
2016-12-13 11:34:58 -0600

    Create ITfb.js

---
+++ js/ITfb.js
@@ -0,0 +1,11 @@
+console.log('Im working')
+
+function injectCSS(link) {
+    var head = document.getElementsByTagName('head')[0]
+    var myCSS = document.createElement('link')
+    myCSS.href = link
+    myCSS.rel = 'stylesheet'
+    myCSS.type = 'text/css'
+    head.appendChild(myCSS)
+}
+injectCSS('https://dbrewitz.github.io/css/ITfb.css')
